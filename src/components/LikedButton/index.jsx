edefcd71cc843beebf6fb7cc2a184569d31015a7
noodle <noodle>
2023-08-25 02:08:16 +0900

    feature: LikedButton, Modal 생성

---
+++ src/components/LikedButton/index.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+
+import style from './index.module.scss';
+
+export const LikedButton = ({ targetId, myPokemon }) => {
+  const LOCAL_KEY_MYMONSTER = 'myMonster';
+  const localData = localStorage.getItem(LOCAL_KEY_MYMONSTER);
+  const parsedData = JSON.parse(localData) || [];
+
+  const initialLiked = parsedData.find(item => item && item.id === targetId)?.liked || false;
+  const [isLiked, setIsLiked] = useState(initialLiked);
+
+  useEffect(() => {
+    const updateData = parsedData.map(item => {
+      if (item && item.id === targetId) {
+        return { ...item, liked: isLiked };
+      }
+      return item;
+    });
+    localStorage.setItem(LOCAL_KEY_MYMONSTER, JSON.stringify(updateData));
+  }, [isLiked, parsedData]);
+
+  const handleLiked = e => {
+    e.stopPropagation();
+    setIsLiked(prev => !prev);
+  };
+
+  return (
+    <button className={`${style.liked} ${isLiked ? style.isClicked : ''}`} onClick={handleLiked}>
+      {isLiked ? '❤️' : '🤍'}
+    </button>
+  );
+};
---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from './index.module.scss';
+
+export const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div onClick={onClose} className={style.modal}>
+      <div className={style.modal_inner}>
+        <button onClick={onClose} className={style.close_btn}>
+          <span className={style.close_icon}></span>
+        </button>
+        <div className={style.modal_content}>{children}</div>
+      </div>
+    </div>
+  );
+};
