d63652a50971306f46c8b1155467dc6fa4657db0
noodle <noodle>
2023-08-25 02:07:41 +0900

    feature: PokemonBook 모달, 검색 필터링 WIP

--- src/components/PokemonBook/index.jsx
+++ src/components/PokemonBook/index.jsx
@@ -1,31 +1,111 @@
 import { Navigate, useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
+import { LikedButton } from '../LikedButton';
+import { Modal } from '../Modal';
+import { PokemonType } from '@components/PokemonType';
+import { debounce } from 'lodash';
 import style from './index.module.scss';
 
 export const PokemonBook = () => {
   const navigate = useNavigate();
-  const myPokemon = JSON.parse(localStorage.getItem('myMonster'));
+  const initialData = JSON.parse(localStorage.getItem('myMonster'));
+  const [myPokemon, setMyPokemon] = useState(initialData);
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const [searchKeyword, setSearchKeyword] = useState('');
 
-  const handleModal = () => {
-    console.log('click');
+  const handleModal = (id, name) => {
+    const liked = () => myPokemon.find(p => p.id === id);
+    setSelectedPokemon(liked);
   };
 
+  const debouncedHandleSearch = useCallback(
+    debounce(keyword => {
+      if (searchKeyword === keyword) return;
+      setSearchKeyword(keyword);
+    }, 100),
+    [searchKeyword],
+  );
+
+  const handleSearch = useCallback(
+    e => {
+      const keyword = e.target.value;
+      debouncedHandleSearch(keyword);
+    },
+    [searchKeyword],
+  );
+
+  const filterPokemonList = () => {
+    if (!searchKeyword) return myPokemon;
+    return myPokemon.filter(pokemon => pokemon.name.toLowerCase().includes(searchKeyword.toLowerCase()));
+  };
+
+  const filteredData = filterPokemonList(searchKeyword);
+
+  useEffect(() => {
+    if (!searchKeyword || searchKeyword.length === 0) {
+      setMyPokemon(initialData);
+    } else {
+      setMyPokemon(filteredData);
+    }
+  }, [searchKeyword]);
+
   return (
-    <div>
-      <button onClick={() => navigate('/')}>포켓몬 뽑으러가기</button>
+    <div className={style.myPokemon}>
+      <button onClick={() => navigate('/')}>포켓몬 잡으러 가기</button>
       <h1>내 포켓몬 북</h1>
-      <p>얘네.. 다 내꺼야..!</p>
-      <ul className={style.myPokemon_list}>
-        {Array.from(myPokemon).map(mon => {
-          return (
-            <li key={mon.id} className={style.myPokemon_item} onClick={handleModal}>
-              <img src={mon.url} alt="" />
-              <span className={style.myPokemon_name}>{mon.name}</span>
-            </li>
-          );
-        })}
-      </ul>
+      <div className={style.boox_utils}>
+        <input
+          type="text"
+          placeholder="검색"
+          value={searchKeyword}
+          onChange={handleSearch}
+          style={{ border: '2px solid black', padding: '12px 30px', borderRadius: '20px' }}
+        />
+        <select>
+          <option value="좋아요 순">좋아요 순</option>
+          <option value="포획 순">포획 순</option>
+          <option value="유형">유형</option>
+          <option value="성별">성별</option>
+        </select>
+      </div>
+      {myPokemon ? (
+        <ul className={style.myPokemon_list}>
+          {Array.from(myPokemon).map(mon => {
+            return (
+              <li key={mon.id} className={style.myPokemon_item} onClick={() => handleModal(mon.id, mon.name)}>
+                <img src={mon.url} alt={mon.name} />
+                <PokemonType key={mon.id} type={mon.types[0].type.name} styles={style.myPokemon_name}>
+                  {mon.name}
+                </PokemonType>
+                <LikedButton myPokemon={myPokemon} targetId={mon.id} />
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <div className={style.myPokemon_no_list}>
+          <strong>잡은 포켓몬이 없어요!</strong>
+          <button onClick={() => navigate('/')}>포켓몬 잡으러 가기</button>
+        </div>
+      )}
+
+      <Modal isOpen={selectedPokemon !== null} onClose={() => setSelectedPokemon(null)}>
+        {selectedPokemon !== null && (
+          <>
+            <div className={style.img_box}>
+              <img src={selectedPokemon.url} alt={selectedPokemon.name} className={style.img} />
+            </div>
+            <div className={style.img_info}>
+              <h2>{selectedPokemon.name}</h2>
+              <div>설명</div>
+              <div>스탯</div>
+              <div>진화</div>
+              <div>스킬</div>
+            </div>
+          </>
+        )}
+      </Modal>
     </div>
   );
 };
