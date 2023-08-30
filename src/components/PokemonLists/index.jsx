69d3e13b6e6b555b9dd37ce163cbe7b868f0431e
noodle <noodle>
2023-08-31 01:31:07 +0900

    feature: 포켓몬도감 페이지 WIP

---
+++ src/components/PokemonLists/index.jsx
@@ -0,0 +1,188 @@
+import React, { useEffect, useState } from 'react';
+
+import { ErrorComponent } from '@components/ErrorComponent';
+import { LoadingComponent } from '@components/LoadingComponent';
+import { PokemonType } from '@components/PokemonType';
+import axios from 'axios';
+import { formatNumber } from '@/utils/utils';
+import { instance } from '@/api/axiosConfig';
+import style from './index.module.scss';
+import { useQuery } from 'react-query';
+
+const fetchPokemonByRange = async (page, pageSize) => {
+  const limitParams = {
+    offset: (page - 1) * pageSize,
+    limit: pageSize,
+  };
+
+  try {
+    const [pokeResponse, speciesResponse] = await Promise.all([
+      instance.get(`/pokemon`, { params: limitParams }),
+      instance.get(`/pokemon-species`, { params: limitParams }),
+    ]);
+
+    const resultPoke = pokeResponse.data.results;
+    const resultSpecies = speciesResponse.data.results;
+
+    return [resultPoke, resultSpecies];
+  } catch (error) {
+    console.log(error);
+    throw new Error('Failed to "fetchPokemonByRange"');
+  }
+};
+
+export const PokemonLists = () => {
+  const pageSize = 1;
+  const {
+    data: [resultPoke, resultSpecies] = [[], []],
+    isLoading,
+    isError,
+    error,
+  } = useQuery(['allPokemon'], async () => fetchPokemonByRange(1, pageSize), {
+    keepPreviousData: true,
+  });
+
+  const [pokeData, setPokeData] = useState([]);
+  const [speciesData, setSpeciesData] = useState([]);
+  const [allPokeData, setAllPokeData] = useState([]);
+
+  useEffect(() => {
+    const fetchDataDetails = async () => {
+      const isExist = resultPoke?.length > 0 && resultSpecies?.length > 0;
+
+      try {
+        if (isExist) {
+          const pokePromises = resultPoke.map(pokemon => axios.get(pokemon.url));
+          const speciesPromises = resultSpecies.map(pokemon => axios.get(pokemon.url));
+
+          const pokeResponses = await Promise.all(pokePromises);
+          const speciesResponses = await Promise.all(speciesPromises);
+
+          const pokeData = pokeResponses.map(response => response.data);
+          const speciesData = speciesResponses.map(response => response.data);
+
+          return { pokeData, speciesData };
+        }
+      } catch (error) {
+        console.log(error);
+        throw new Error('Failed to "fetchDataDetails"');
+      }
+    };
+
+    fetchDataDetails().then(({ pokeData, speciesData }) => {
+      setPokeData(pokeData);
+      setSpeciesData(speciesData);
+
+      const mergedData = pokeData.map(poke => {
+        const correspondingSpecie = speciesData.find(species => species.name === poke.name && species.id === poke.id);
+        return { ...poke, ...correspondingSpecie };
+      });
+
+      setAllPokeData(mergedData);
+    });
+  }, [isLoading]);
+
+  if (isLoading) return <LoadingComponent loadingMessage={'포켓몬 불러오는 중'} />;
+  if (isError) return <ErrorComponent errorMessage={error.message} />;
+
+  return (
+    <div>
+      <h1>포켓몬 도감</h1>
+      <ul className={style.list}>
+        {allPokeData.map(pokemon => {
+          const {
+            id,
+            names,
+            types,
+            weight,
+            height,
+            stats,
+            abilities,
+            shape,
+            flavor_text_entries,
+            genera,
+            color,
+            sprites,
+          } = pokemon;
+
+          const statsss = stats.map(stat => {
+            // {hp: 45},{attack: 49},{defense: 49},{special-attack: 65},{special-defense: 65},{speed: 45}
+            return {
+              [stat.stat.name]: stat.base_stat,
+            };
+          });
+
+          const res = statsss.map((stat, index) => {
+            Object.entries(stat).map(([key, value]) => (
+              <p key={`${key}-${index}`}>
+                `${key}: ${value}`
+              </p>
+            ));
+          });
+
+          const typesResult = types.map(type => type.type.name); //  ['grass', 'poison']
+          const abilityResult = abilities.map(ability => ability.ability.name); //  ['grass', 'poison']
+
+          const filterByLanguage = (arr, lang) => arr.filter(item => item.language.name === lang);
+
+          const selectRandomly = arr => {
+            if (!arr.length) return null;
+
+            const randomIndex = Math.floor(Math.random() * arr.length);
+            return arr[randomIndex];
+          };
+
+          const langFilter = (arr, lang) => {
+            const filteredArr = filterByLanguage(arr, lang);
+            if (!filteredArr.length) return null;
+
+            return selectRandomly(filteredArr);
+          };
+
+          const langFilterAndAccessor = (arr, lang, fieldName) => {
+            const result = langFilter(arr, lang);
+
+            return result ? result[fieldName] : null;
+          };
+
+          const koName = langFilterAndAccessor(names, 'ko', 'name');
+          const koFlavorText = langFilterAndAccessor(flavor_text_entries, 'ko', 'flavor_text');
+          const koGeneraText = langFilterAndAccessor(genera, 'ko', 'genus');
+
+          return (
+            <li key={id} className={style.item}>
+              <div className={style.img_box}>
+                <img className={style.img} src={sprites.other.dream_world.front_default} alt={koName} />
+                <span className={style.id}>#{formatNumber(id, 4)}</span>
+              </div>
+              <div className={style.attr}>
+                <div className={style.img_sub_box}>
+                  <img
+                    className={style.img_sub}
+                    src={sprites.versions[`generation-v`][`black-white`].animated.front_default}
+                    alt={koName}
+                  />
+                  <img
+                    className={style.img_sub}
+                    src={sprites.versions[`generation-v`][`black-white`].animated.back_default}
+                    alt={koName}
+                  />
+                </div>
+                <>
+                  {res}
+                  {(typesResult, abilityResult)}
+                </>
+              </div>
+              <div className={style.desc}>
+                <PokemonType type={types[0].type.name}>{koName}</PokemonType>
+                <p>종류: {koGeneraText}</p>
+                {color.name}
+                <span className={style.flavor_text}>{koFlavorText}</span>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
