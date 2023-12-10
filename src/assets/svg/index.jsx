f6efe8b4a3fd5f1e4a3a395a0213ff1546b86e9d
noodle <noodle>
2023-12-10 22:41:44 +0900

    refactor: 아이콘 경로 추가

--- src/assets/svg/index.jsx
+++ src/assets/svg/index.jsx
@@ -1,4 +1,5 @@
 import { ReactComponent as IconHeart } from '../svg/heart-ico.svg';
 import { ReactComponent as IconHeartFull } from '../svg/heart-full-ico.svg';
+import { ReactComponent as IconSearch } from '../svg/search-ico.svg';
 
-export { IconHeart, IconHeartFull };
+export { IconHeart, IconHeartFull, IconSearch };
