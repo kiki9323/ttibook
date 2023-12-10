3d7f50e6676d191c47cde5f504bb6b1a541b3d3f
noodle <noodle>
2023-12-10 22:58:02 +0900

    chore: vite.config.js ESmodule에서 __dirname 적용 안 되는 것 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
